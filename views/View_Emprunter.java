27f6a60fa8a0eaa0a6489ccb91e3f58459b71cba
Jawed Ouendi <jawedouendi@gmail.com>
2026-03-26 15:44:58 +0100

    Refactor View_Emprunter layout and components

--- views/View_Emprunter.java
+++ views/View_Emprunter.java
@@ -17,6 +17,10 @@ import java.awt.Color;
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Window.Type;
+import java.awt.Dialog.ModalExclusionType;
 
 public class View_Emprunter {
 
@@ -27,18 +31,6 @@ public class View_Emprunter {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					View_Emprunter window5 = new View_Emprunter();
-					window5.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
@@ -55,42 +47,42 @@ public class View_Emprunter {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 642, 395);
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.setBounds(100, 100, 1252, 551);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		Label label = new Label("EMPRUNTER UN LIVRE");
-		label.setBackground(new Color(0, 128, 128));
-		label.setAlignment(Label.CENTER);
-		label.setBounds(144, 35, 302, 22);
-		frame.getContentPane().add(label);
-		
 		Label label_1 = new Label("ISBN :");
+		label_1.setFont(new Font("Segoe Print", Font.PLAIN, 15));
 		label_1.setAlignment(Label.CENTER);
-		label_1.setBounds(52, 89, 62, 22);
+		label_1.setBounds(356, 210, 80, 45);
 		frame.getContentPane().add(label_1);
 		
 		Label label_1_1 = new Label("N° Client :");
+		label_1_1.setFont(new Font("Segoe Print", Font.PLAIN, 15));
 		label_1_1.setAlignment(Label.CENTER);
-		label_1_1.setBounds(52, 129, 62, 22);
+		label_1_1.setBounds(356, 261, 80, 45);
 		frame.getContentPane().add(label_1_1);
 		
 		textField_ISBN = new JTextField();
 		textField_ISBN.setColumns(10);
-		textField_ISBN.setBounds(120, 91, 165, 20);
+		textField_ISBN.setBounds(442, 224, 204, 31);
 		frame.getContentPane().add(textField_ISBN);
 		
 		textField_numClient = new JTextField();
 		textField_numClient.setColumns(10);
-		textField_numClient.setBounds(120, 129, 165, 20);
+		textField_numClient.setBounds(442, 275, 204, 31);
 		frame.getContentPane().add(textField_numClient);
 		
 		Label label_Message = new Label("");
+		label_Message.setFont(new Font("Segoe Print", Font.PLAIN, 15));
+		label_Message.setAlignment(Label.CENTER);
 		label_Message.setVisible(false);
-		label_Message.setBounds(160, 173, 62, 22);
+		label_Message.setBounds(250, 334, 630, 143);
 		frame.getContentPane().add(label_Message);
 		
 		Button button_ok = new Button("OK");
+		button_ok.setFont(new Font("Segoe Print", Font.PLAIN, 20));
 		button_ok.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String isbn = textField_ISBN.getText();
@@ -118,7 +110,35 @@ public class View_Emprunter {
 				
 			}
 		});
-		button_ok.setBounds(330, 105, 70, 22);
+		button_ok.setBounds(703, 224, 91, 46);
 		frame.getContentPane().add(button_ok);
+		
+		JButton btnNewButton = new JButton("Retourner à l'accueil");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(true);
+				frame.dispose();
+				try {
+					View_Accueil window = new View_Accueil();
+					
+				} catch (SQLException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+			}
+		});
+		btnNewButton.setFont(new Font("Segoe Print", Font.PLAIN, 18));
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setBackground(new Color(0, 128, 128));
+		btnNewButton.setBounds(942, 41, 232, 31);
+		frame.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("EMPRUNTER UN LIVRE");
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setBackground(new Color(0, 0, 0));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Segoe Print", Font.PLAIN, 30));
+		lblNewLabel.setBounds(318, 69, 446, 43);
+		frame.getContentPane().add(lblNewLabel);
 	}
 }
